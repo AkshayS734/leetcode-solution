41b4f13684a60a0634d9d4467af1e23419aadd74
Akshay Shukla <manasshukla876@gmail.com>
2024-12-02 00:28:26 +0530

    Group Anagrams added

---
+++ GroupAnagrams.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sortedStr = String.valueOf(arr);
+            map.computeIfAbsent(sortedStr, key -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
