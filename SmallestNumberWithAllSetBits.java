e4ca95232f5fc534d8e735b79f0bc6f0531eae22
Akshay Shukla <manasshukla876@gmail.com>
2024-12-08 17:29:24 +0530

    Smallest Number with All set bits added

---
+++ SmallestNumberWithAllSetBits.java
@@ -0,0 +1,9 @@
+public class SmallestNumberWithAllSetBits {
+    public int smallestNumber(int n) {
+        int res = 1;
+        while (res < n) {
+            res = (res * 2) + 1;
+        }
+        return res;
+    }
+}
