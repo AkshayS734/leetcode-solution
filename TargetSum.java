c1b7b13b6c2c4c3b7bc5cb621c7196c36cf4399f
Akshay Shukla <manasshukla876@gmail.com>
2024-12-26 13:19:52 +0530

    Target Sum added

---
+++ TargetSum.java
@@ -0,0 +1,22 @@
+public class TargetSum {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if ((target + sum) % 2 != 0 || target > sum || target < -sum) {
+            return 0;
+        }
+
+        int subsetSum = (target + sum) / 2;
+        return countSubsets(nums, subsetSum);
+    }
+    private int countSubsets(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+}
