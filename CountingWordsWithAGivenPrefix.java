69ba4f2f9c15031cfefb6c564d47dd0dbd7e164f
Akshay Shukla <manasshukla876@gmail.com>
2025-01-09 18:50:38 +0530

    Counting Words with a given prefix added

---
+++ CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,11 @@
+public class CountingWordsWithAGivenPrefix {
+    public int prefixCount(String[] words, String pref) {
+        int res = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                res++;
+            }
+        }   
+        return res;
+    }
+}
