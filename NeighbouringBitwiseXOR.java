8dbb4ba09cb0b0713746d3ac311f6ff4752f3c76
Akshay Shukla <manasshukla876@gmail.com>
2025-01-17 22:59:38 +0530

    Neighbouring Bitwise XOR added

---
+++ NeighbouringBitwiseXOR.java
@@ -0,0 +1,8 @@
+public class NeighbouringBitwiseXOR {
+    public boolean doesValidArrayExist(int[] derived) {
+        for (int i = 1; i < derived.length; i++) {
+            derived[0] ^= derived[i];
+        }
+        return derived[0] == 0;
+    }
+}
